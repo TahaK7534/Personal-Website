5ff324d09f89bac025600badea78bff8cfe4c6c6
TahaK7534 <tahakhan7534@gmail.com>
2025-12-29 01:56:42 -0700

    Centered The selected card

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 /*
 Next time, when you click on any of the cards center the card that you clicked
-- make all other cards fly out to the left and right respectively animate that
-- center the clicked card animate that
+- make all other cards fly out to the left and right respectively animate that -- DONE
+- center the clicked card animate that 
 -  perform a zoom in style animation on the card
-- that should take you the project page
+- that should take you the project page -- I should switch to using node.js and react 
 - There needs to be a reverse type of animation for leaving the page
 Also leave room for customization of stuff, like the colors of the background when zomming in - this is mainly so that
 I can make it more class like so adding projects in the future is as simple as possible.
+
+
+Make it so you can only click the image when they are fully in view - but like what if they are not fully in view??? - 
+
+fix when you click the button move the scroll to the right location and stop you from scrolling
+
+
+Then I should Make the final Page aka the about me page aka the red area
+make like three options in the center (about me, education, work experience)
+- Make the about me page - Make this one more fun
+- Make the education page - Cute little animations in and arround the text would be cool
+- Make the work experience page - Same thing here
 */
 
 
@@ -25,14 +37,11 @@ for (const image of Card_Images) {
 }
 
 
-
 function CardSelected(event) {
     const SelectedImage = event.currentTarget;
     CardFlyingAwayAnimation(SelectedImage.id)
+    CenterCard(SelectedImage)
 }
-
-
-
 
 
 function CardFlyingAwayAnimation(SelectedImageId) {
@@ -41,14 +50,7 @@ function CardFlyingAwayAnimation(SelectedImageId) {
             const SelectedImageIDNumber = SelectedImageId.substr(SelectedImageId.length - 1);
             const ImageIdNumber = image.id.substr(image.id.length - 1);
 
-            //Get the current computed styles (where the scroll animation is currently)
-            const currentTransform = window.getComputedStyle(image).transform;
-            const currentOpacity = window.getComputedStyle(image).opacity;
-
-            // Apply these as inline styles so they persist after removing other animation class
-            image.style.transform = currentTransform;
-            image.style.opacity = currentOpacity;
-
+            PersistCurrentStyle(image)
             // Remove the scroll animation class becasue If I keep this this animation will play again
             image.classList.remove("Image-Down");
 
@@ -62,4 +64,36 @@ function CardFlyingAwayAnimation(SelectedImageId) {
     });
 }
 
+function CenterCard(SelectedImage) {
+    PersistCurrentStyle(SelectedImage);
 
+    SelectedImage.classList.remove("Image-Down");
+    const rect = SelectedImage.getBoundingClientRect();
+    const viewportCenterX = window.innerWidth / 2;
+    const imageCenterX = rect.left + rect.width / 2;
+    const deltaX = viewportCenterX - imageCenterX;
+
+    // Animate to center
+    SelectedImage.animate(
+        [
+            { transform: SelectedImage.style.transform },
+            { transform: `translateX(${deltaX}px)` }
+        ],
+        {
+            duration: 3000,
+            easing: "ease-out",
+            fill: "forwards"
+        }
+    );
+}
+
+
+
+
+function PersistCurrentStyle(image){
+        const currentTransform = window.getComputedStyle(image).transform;
+        const currentOpacity = window.getComputedStyle(image).opacity;
+        // Apply these as inline styles so they persist after removing other animation class
+        image.style.transform = currentTransform;
+        image.style.opacity = currentOpacity;
+}
